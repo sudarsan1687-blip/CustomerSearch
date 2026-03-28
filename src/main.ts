99fb3954d53fc8dc7e6432068c2260ca6f472287
Sudarsan Murthy Dhandapani <sudarsan@blocked.local>
2026-03-28 11:27:03 +0100

    Trigger redeploy

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+// Deployment trigger Sat Mar 28 11:27:03 CET 2026
